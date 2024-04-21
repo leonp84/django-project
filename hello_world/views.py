0646d378aa8d152163355341999086204403a956
Leon Potgieter <hexvallei@gmail.com>
2024-04-21 14:39:59 +0000

    create about app

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world! My first Django app... but not the last!")
+    return HttpResponse("Hello,world! My first' \
+                        'Django app... but not the last!")
--- my_project/urls.py
+++ my_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # noqa
 from hello_world import views as index_views
 
 urlpatterns = [
