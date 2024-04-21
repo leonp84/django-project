41008d356322a38102c463d82e39f43ad16d369c
Leon Potgieter <hexvallei@gmail.com>
2024-04-21 14:02:57 +0000

    initial commit

---
+++ hello_world/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render # noqa
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello,world! My first Django app... but not the last!")
